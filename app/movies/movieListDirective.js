d79610c0db5aa5252bb4a2e04ded9cee27d8c964
Walter Holladay <walter@holladays.org>
2015-03-14 07:47:53 -0600

    Take "None" genre into account when sorting.

--- app/movies/movieListDirective.js
+++ app/movies/movieListDirective.js
@@ -33,6 +33,7 @@ myApp.directive('movieList', ['$filter', 'MovieService', function($filter, Movie
             function compareByGenre(movie1, movie2) {
                 var result = movie1.genreName.localeCompare(movie2.genreName);
                 if (!result) {
+                    // Genres are the same, so we need to look at sub genres...
                     result = movie1.subGenreName.localeCompare(movie2.subGenreName);
                     if (!result) {
                         result = movie1.title.localeCompare(movie2.title);
@@ -41,6 +42,14 @@ myApp.directive('movieList', ['$filter', 'MovieService', function($filter, Movie
                         }
                         return result;
                     }
+                    // Sub genres are different so we check to see if movie1 has no sub genre.
+                    if (movie1.subGenre === 1) {
+                        return -1;
+                    }
+                    // Now see if movie2 has a sub genre of "None"
+                    if (movie2.subGenre === 1) {
+                        return 1;
+                    }
                     return result;
                 }
                 return result;
@@ -48,10 +57,11 @@ myApp.directive('movieList', ['$filter', 'MovieService', function($filter, Movie
 
             function sortByGenre() {
                 if (scope.movies.length > 0 && scope.orderSetting === 'genre') {
-                    // First filter the list of movies.
+                    // First, filter the list of movies.
                     var filteredMovies = $filter('filter')(scope.movies, scope.criteriaMatch());
-                    // Then sort the filtered movies by genre.
+                    // Second, sort the filtered movies by genre.
                     filteredMovies.sort(compareByGenre);
+                    // Finally, divide the list into genre groups so that a heading can be displayed for each genre.
                     scope.moviesByGenre = [];
                     var currentGroup = {name: null};
                     angular.forEach(filteredMovies, function(movie) {
