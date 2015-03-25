38e162efea2cc175dd15e907872237d886f2dc55
Walter Holladay <walter@holladays.org>
2015-03-25 09:57:59 -0600

    Added some e2e tests.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,41 +1,80 @@
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 /* global describe, browser, it, expect, beforeEach, element, by */
-describe('my app', function() {
+describe('movies', function() {
     'use strict';
 
-    browser.get('index.html');
-
-    it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-        expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    beforeEach(function() {
+        browser.get('/');
     });
 
+    function getMovies() {
+        return element.all(by.repeater('movie in movies'));
+    }
 
-    describe('view1', function() {
+    describe('default list', function() {
 
-        beforeEach(function() {
-            browser.get('index.html#/view1');
+        it('should list all movies', function() {
+            expect(browser.getTitle()).toEqual('My AngularJS App');
+            expect(getMovies().count()).toBe(429);
         });
-
-
-        it('should render view1 when user navigates to /view1', function() {
-            expect(element.all(by.css('[ng-view] p')).first().getText()).
-                    toMatch(/partial for view 1/);
-        });
-
     });
 
+    describe('order by', function() {
 
-    describe('view2', function() {
+        it('should render all movies sorted by number', function() {
+            expect(browser.getTitle()).toEqual('My AngularJS App');
+            var orderBy = element(by.model('mc.orderBy'));
+            expect(orderBy).toBeDefined();
+            orderBy.click();
+            var numberOption = element(by.id('select_option_00D'));
+            expect(numberOption).toBeDefined();
+            numberOption.click();
 
-        beforeEach(function() {
-            browser.get('index.html#/view2');
+            var movies = getMovies();
+            expect(movies.count()).toBe(429);
+            movies.first().getText().then(function(data) {
+                expect(data.indexOf('1')).toBe(0);
+            });
+            movies.last().getText().then(function(data) {
+                expect(data.indexOf('429')).toBe(0);
+            });
         });
 
+        it('should render all movies sorted by title', function() {
+            expect(browser.getTitle()).toEqual('My AngularJS App');
+            var orderBy = element(by.model('mc.orderBy'));
+            expect(orderBy).toBeDefined();
+            orderBy.click();
+            var titleOption = element(by.id('select_option_00C'));
+            expect(titleOption).toBeDefined();
+            titleOption.click();
 
-        it('should render view2 when user navigates to /view2', function() {
-            expect(element.all(by.css('[ng-view] p')).first().getText()).
-                    toMatch(/partial for view 2/);
+            var movies = getMovies();
+            expect(movies.count()).toBe(429);
+            movies.first().getText().then(function(data) {
+                expect(data.indexOf('101 Dalmatians')).toBeGreaterThan(0);
+            });
+            movies.last().getText().then(function(data) {
+                expect(data.indexOf('Yours, Mine & Ours')).toBeGreaterThan(0);
+            });
         });
-
     });
+
+    describe('criteria', function() {
+
+        it('should filter by title', function() {
+            var titleInput = element(by.model('mc.criteria.title'));
+            expect(titleInput).toBeDefined();
+            titleInput.sendKeys('hunger');
+            expect(getMovies().count()).toBe(1);
+        });
+
+        it('should filter by genre', function() {
+            var genreInput = element(by.model('mc.criteria.genre'));
+            expect(genreInput).toBeDefined();
+            genreInput.sendKeys('western');
+            expect(getMovies().count()).toBe(7);
+        });
+    });
+
 });
